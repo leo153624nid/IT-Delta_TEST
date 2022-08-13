5d18a4fd4b33a49d8c854c087d38637349490648
leo153624nid <barjastroy@gmail.com>
2022-08-13 14:45:08 +0300

    Create userSlice.ts

---
+++ src/store/slices/userSlice.ts
@@ -0,0 +1,34 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+// interface InitState {
+//     email: string | null
+//     token: string | null
+//     id: string | null
+// }
+
+// const initialState: InitState = {
+//     email: null,
+//     token: null,
+//     id: null,
+// }
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        setUser(state, action: PayloadAction<InitState>) {
+            state.email = action.payload.email
+            state.token = action.payload.token
+            state.id = action.payload.id
+        },
+        removeUser(state) {
+            state.email = null
+            state.token = null
+            state.id = null
+        },
+    },
+})
+
+export const { setUser, removeUser } = userSlice.actions
+
+export default userSlice.reducer
