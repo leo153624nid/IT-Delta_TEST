ad67f4828e2e6de5bb035e8934d6e16f491bd09d
leo153624nid <barjastroy@gmail.com>
2022-08-13 21:17:11 +0300

    Update ImageList.tsx

--- src/components/ImageList/ImageList.tsx
+++ src/components/ImageList/ImageList.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-alert */
+/* eslint-disable no-console */
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable jsx-a11y/control-has-associated-label */
@@ -5,15 +7,27 @@ import React from 'react'
 import s from './ImageList.module.scss'
 import { Image } from '../../store/slices/userSlice'
 import Card from '../Card/Card'
+import userDataAPI from '../../api/userDataAPI'
 
 interface ImageListProps {
     images: Image[]
 }
 
 function ImageList({ images }: ImageListProps) {
+    const getImgData = async (imageId: number) => {
+        try {
+            const data = await userDataAPI.getImageData(imageId)
+            console.dir(data)
+        } catch (error) {
+            console.log(error)
+            alert('Данные не получены')
+        }
+    }
+
+    // Создаем массив карточек-фотографий
     const imagesList = images.map((item) => (
         <div key={item.id} className={s.cell}>
-            <Card url={item.url} id={item.id} />
+            <Card url={item.url} id={item.id} getImgData={getImgData} />
         </div>
     ))
     return <div className={s.imageList}>{imagesList}</div>
