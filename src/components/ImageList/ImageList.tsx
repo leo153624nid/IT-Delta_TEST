523e9eee0752e88b12d5b63c8dd5bfe6b35b7b5b
leo153624nid <barjastroy@gmail.com>
2022-08-13 15:20:31 +0300

    Create ImageList.tsx

---
+++ src/components/ImageList/ImageList.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React from 'react'
+import s from './ImageList.module.scss'
+
+// interface FormProps {
+//     title: string
+//     emailMemo: string
+//     passMemo: string
+//     // eslint-disable-next-line no-unused-vars
+//     handleClick: (email: string, pass: string, memo: boolean) => void
+// }
+
+function ImageList() {
+    return (
+        <div>
+            <div>1</div>
+            <div>2</div>
+        </div>
+    )
+}
+
+export default ImageList
