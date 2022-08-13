7cb3a749f58d85bd49801007be3725953a7921f9
leo153624nid <barjastroy@gmail.com>
2022-08-13 15:20:27 +0300

    Create Header.tsx

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React, { useState } from 'react'
+import s from './Header.module.scss'
+import banner from '../../img/banner.svg'
+
+// interface FormProps {
+//     title: string
+//     emailMemo: string
+//     passMemo: string
+//     // eslint-disable-next-line no-unused-vars
+//     handleClick: (email: string, pass: string, memo: boolean) => void
+// }
+
+function Header() {
+    return (
+        <header>
+            <div className={s.banner}>
+                <img src={banner} alt="banner" />
+            </div>
+
+            <div className={s.user}>
+                <div>avatar</div>
+
+                <div className={s.userData}>
+                    <div className={s.userName}>userName</div>
+
+                    <div className={s.userButtons}>
+                        <button type="button" className={s.message}>
+                            Message
+                        </button>
+                        <button type="button" className={s.call}>
+                            Call
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
