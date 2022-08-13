4567b086e19251b073cef80f8d9bab1e416c930a
leo153624nid <barjastroy@gmail.com>
2022-08-13 14:44:50 +0300

    Update constants.ts

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,17 +1,17 @@
-// export const LOGIN = 'login'
-// export const CREATE_NEW_ACCOUNT = 'create new account'
-// export const RESET_PASSWORD = 'reset password'
+export const LOGIN = 'login'
+export const CREATE_NEW_ACCOUNT = 'create new account'
+export const RESET_PASSWORD = 'reset password'
 
-// export const emailValidator = {
-//     isEmpty: true,
-//     emailError: true,
-// }
+export const emailValidator = {
+    isEmpty: true,
+    emailError: true,
+}
 
-// export const passValidator = {
-//     isEmpty: true,
-//     passError: true,
-// }
+export const passValidator = {
+    isEmpty: true,
+    passError: true,
+}
 
-// export type ValidatorEmail = typeof emailValidator
-// export type ValidatorPass = typeof passValidator
-// export type Validator = ValidatorEmail | ValidatorPass
+export type ValidatorEmail = typeof emailValidator
+export type ValidatorPass = typeof passValidator
+export type Validator = ValidatorEmail | ValidatorPass
