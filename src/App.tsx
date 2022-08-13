bf6788c62883bba6f01173c01e50a7387663fa35
leo153624nid <barjastroy@gmail.com>
2022-08-13 16:48:45 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
-import './App.scss'
+import userDataAPI from './api/userDataAPI'
+import s from './App.module.scss'
 import HomePage from './pages/HomePage/HomePage'
+import { useAppDispatch } from './store/hooks/hooks'
+import { setUser } from './store/slices/userSlice'
 
 function App() {
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        // Т.к. в задании не предполагается изменение или получение user'a,
+        // то передается пустая строка вместо userId
+        userDataAPI.getUserData('').then((data) => {
+            dispatch(
+                setUser({
+                    userName: 'Ricardo Cooper',
+                    images: [...data],
+                })
+            )
+        })
+    }, [])
     return (
-        <div className="App">
+        <div className={s.App}>
             <Routes>
                 <Route path="/" element={<HomePage />} />
             </Routes>
